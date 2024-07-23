165a1f25e944a718c087045cc4631db3a7ca787e
lauraoliveri <lauraoliveri2003@gmail.com>
2024-07-23 17:17:22 +0200

    Edit js file

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+// crea bottone 
+const PlayButton = document.getElementById('play_btn');
+
+// quando lìutente clicca il bottone allora
+
+PlayButton.addEventListener('click', function() {
+
+    // contenitore della griglia
+    const Grid = document.getElementById('grid_container');
+    console.log('Grid', Grid, typeof Grid);
+
+    for(let i = 0; i < 100; i++) {
+
+        const NewCell = document.createElement('div');
+        NewCell.classList.add("grid_container_cells");
+        console.log('Nuova Cella', NewCell, typeof NewCell);
+        NewCell.innerHTML =  i + 1 ;
+
+        // aggiungo le nuova celle al contenitore della griglia
+        Grid.append(NewCell);
+        
+        NewCell.addEventListener('click', function() {
+            NewCell.classList.add("cells_clicked");
+            console.log(NewCell.innerHTML);
+        })
+
+
+    }
+
+        
+
+    for( let i = 0; i < 16; i++) {
+        
+        const BombCells = Math.floor((Math.random() * 16) + 1);
+        const BombsArr = [Bombcells];
+        
+        const RandomCellChoice = BombsArr[Math.floor(Math.random() * BombsArr.length)];  
+        BombsArr[Bombs];
+        Grid.append(Bombs);
+
+        Bombs.addEventListener('click', function() {
+            Bombs.classList.add("bombs");
+            console.log(Bombs.innerHTML);
+        })
+
+       
+    }
+
+})
+ 
+
+
+
